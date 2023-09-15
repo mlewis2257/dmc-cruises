4940e238178d3520b38a962abe143362accf8cc5
cblake003 <cameronburtner@gmail.com>
2023-09-15 14:08:18 -0700

    created Cruise model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cruise(models.Model):
+    name = models.CharField()
+    duration = models.IntegerField()
+    sale_from = models.CharField()
+    destinations = models.ManyToManyField(Destination)
