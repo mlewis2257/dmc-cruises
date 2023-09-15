1bc8236d0545919b957ccd37e1370b024934c767
cblake003 <cameronburtner@gmail.com>
2023-09-15 14:21:04 -0700

    added two models

--- main_app/models.py
+++ main_app/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Destination(models.Model):
+    excursions = models.CharField()
+    location = models.CharField()
+    
+
 class Cruise(models.Model):
     name = models.CharField()
     duration = models.IntegerField()
     sale_from = models.CharField()
     destinations = models.ManyToManyField(Destination)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
