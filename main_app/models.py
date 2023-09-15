b8ec5ad5344f53bacd91255a9f7d2cbcb8b9da57
cblake003 <cameronburtner@gmail.com>
2023-09-15 15:23:04 -0700

    created options for rooms and location

--- main_app/models.py
+++ main_app/models.py
@@ -3,10 +3,25 @@ from django.urls import reverse
 
 # Create your models here.
 
+ROOM_TYPE = (
+    ('I', 'Interior'),
+    ('O', 'Ocean View'),
+    ('B', 'Balcony'),
+    ('JS', 'Junior Suite'),
+    ('S', 'Suite'),
+)
+
+LOCATION = (
+    ('P', 'Portside'),
+    ('S', 'Starboard Side'),
+    ('F', 'Front of Ship'),
+    ('BS'. 'Back of Ship')
+)
+
+
 class Destination(models.Model):
     excursions = models.CharField(max_length=500)
     location = models.CharField(max_length=250)
-    cruise = models.ForeignKey(Cruise, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -28,14 +43,27 @@ class Cruise(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'cruise_id': self.id})
 
-class Room(model.Models):
-    type = models.TextField()
-    number = models.IntegerField()
-    location = models.TextField()
-    booking = models.ForeignKey(Room, on_delete=models.CASCADE)
+class Room(models.Model):
+    type = models.CharField(
+        max_length=50,
+        choices=ROOM_TYPE,
+        default=ROOM_TYPE[0][0],
+    )
+    number = models.IntegerField(
+        min_num=100,
+        max_num=500
+    )
+    location = models.CharField(
+        max_length=50,
+        choices=LOCATION,
+        default=LOCATION[0][0]
+        )
 
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.get_room_display()} on {self.date}"
 
-class User(model.Models):
+class User(models.Model):
     username = models.CharField(max_length=25)
     password = models.CharField(max_length=50)
     email = models.TextField()
@@ -43,9 +71,8 @@ class User(model.Models):
     last_name = models.TextField()
 
 
-class Booking(model.Models):
+class Booking(models.Model):
     cruises = models.OneToManyField(cruise)
     users = models.OneToManyField(user)
     destinations = models.ManyToManyField(destination)
-    rooms = models.OneToManyField(room)
     price = models.IntegerField()
