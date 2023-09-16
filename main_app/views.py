54d8caf4099511747d5c6e4238f28c962f08a373
mlewis2257 <mlang.lewis@gmail.com>
2023-09-15 21:50:14 -0700

    views and date to booking

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Cruise, Destination
+from .models import Cruise, Destination, Booking
 
 # Create your views here.
 
@@ -17,8 +17,18 @@ def cruises_index(request):
     })
 
 
+def cruise_detail(request, cruise_id):
+    cruise = Cruise.objects.get(id=cruise_id)
+    return render(request, 'cruises/detail.html', {'cruise': cruise})
+
+
 def destinations_index(request):
     destinations = Destination.objects.all()
     return render(request, 'destinations/index.html', {
         'destinations': destinations
     })
+
+
+class BookingCreate(CreateView):
+    model = Booking
+    fields = '__all__'
